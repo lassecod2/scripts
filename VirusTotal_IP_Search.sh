2d61bb05c42c6af741a9c1d01ae5dd1eca3947e0
Lasse Martin Moisen <39300644+lassecod2@users.noreply.github.com>
2021-09-21 18:43:59 +0200

    Update VirusTotal_IP_Search.sh

--- VirusTotal_IP_Search.sh
+++ VirusTotal_IP_Search.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# A curl GET request to fetch json data on the IP address I submit.
+# ./VirusTotal_IP_Search.sh "<IP address>"
 curl --request GET \
     --url https://www.virustotal.com/api/v3/ip_addresses/$1 \
     --header 'x-apikey: <API-KEY>'
