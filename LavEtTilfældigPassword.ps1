77dfa27736649b4a0ca3c99b67bba29047b4805e
Lasse Martin Moisen <39300644+lassecod2@users.noreply.github.com>
2021-09-21 18:23:21 +0200

    Add files via upload

---
+++ LavEtTilfældigPassword.ps1
@@ -0,0 +1,23 @@
+﻿function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs=""
+    return [String]$characters[$random]
+}
+ 
+function Scramble-String([string]$inputString){     
+    $characterArray = $inputString.ToCharArray()   
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+    $outputString = -join $scrambledStringArray
+    return $outputString 
+}
+ 
+$password = Get-RandomCharacters -length 8 -characters 'abcdefghikmnoprstuvwxyz'
+$password += Get-RandomCharacters -length 2 -characters 'ABCDEFGHKLMNPRSTUVWXYZ'
+$password += Get-RandomCharacters -length 2 -characters '1234567890'
+$password += Get-RandomCharacters -length 2 -characters '!#,.@'
+ 
+Write-Host $password
+ 
+$password = Scramble-String $password
+ 
+Write-Host $password
